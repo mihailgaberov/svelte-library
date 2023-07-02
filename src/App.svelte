9acbcadbf356bf8dab2c8b3075612a8f604859ab
Mihail Gaberov <mihail.gaberov@olx.com>
2023-07-02 14:09:33 +0200

    :tada: Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Library from "./library/Library.svelte"
+</script>
+
+<main>
+	<Library />
+</main>
+
+<style>
+	
+</style>
---
+++ src/library/Library.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Button from "../common/Button.svelte";
+</script>
+
+<header>
+    <span class="preamble">Welcome to the</span><h1>Library</h1>
+</header>
+
+<p class="greeting">This is a library for the people. Welcome. Read the books here. Be inspired. Go home and share them with your family.</p>
+<Button>+ Add Book</Button>
+<style>
+      header {
+    margin: var(--spacingMedium) 0 var(--spacingLarge) 0;
+    text-transform: uppercase;
+  }
+  .preamble {
+    display: block;
+  }
+  h1 {
+    font-size: var(--typeSizeXXLarge);
+    font-weight: var(--typeWeightBold);
+    line-height: var(--typeLineHeightTight);
+  }
+
+  .greeting {
+    font-size: var(--typeSizeSmall);
+  }
+
+</style>
